9322ff37eef3d769ec45c965633b2cb05be1cf1b
Atsukoro1 <dornicakkuba@gmail.com>
2022-10-07 20:55:54 +0200

    Add animations, other sections and remove some useless motion components (replace them with standard html tags)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,28 @@
+import { motion, Variants } from "framer-motion"
 import Shade from "../components/Shade"
-import { motion } from "framer-motion"
 import ReactPlayer from "react-player"
 
 const Main = () => {
+  function backToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  const describeVariants: Variants = {
+    offscreen: {
+      translateY: -150, 
+      opacity: 0,
+    },
+    onscreen: {
+      translateY: 0,
+      opacity: 1,
+      transition : {
+        duration: 0.3,
+        type: "spring",
+        stiffness: 100
+      }
+    }
+  };
+
   return (
     <div 
       style={{ backgroundImage: 'url("/assets/background.webp")', fontFamily: "Lora" }}
@@ -19,7 +39,7 @@ const Main = () => {
         })
       }
 
-      <motion.header className="relative z-10 w-[1000px] mr-auto ml-auto text-center mt-[-750px]">
+      <header className="relative z-10 w-[1000px] mr-auto ml-auto text-center mt-[-750px]">
         <motion.p 
           className="text-white tracking-[4px] text-[30px] mt-20"
           initial={{ opacity: 0 }}
@@ -70,12 +90,18 @@ const Main = () => {
             loop={true}
           />
         </motion.div>
-      </motion.header>
+      </header>
 
-      <motion.section className="text-[#857e71] mr-auto ml-auto w-[900px] text-center mt-20 mb-20">
-        <motion.h1 className="text-6xl">Lorem Ipsum</motion.h1>
-        <motion.div className="bg-[#857e71] inline-block w-[60px] h-1 mt-10">&nbsp;</motion.div>
-        <motion.p className="mt-3 w-[700px] mr-auto ml-auto text-lg font-helvetica">
+      <motion.section 
+        className="text-[#857e71] mr-auto ml-auto w-[900px] text-center mt-20 mb-20"
+        initial="offscreen"
+        whileInView="onscreen"
+        variants={describeVariants}
+        viewport={{ once: false, amount: 0.8 }}
+      >
+        <h1 className="text-6xl">Lorem Ipsum</h1>
+        <div className="bg-[#857e71] inline-block w-[60px] h-1 mt-10">&nbsp;</div>
+        <p className="mt-3 w-[700px] mr-auto ml-auto text-lg font-helvetica">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
           Ducimus deleniti dicta corrupti eligendi, minima, ipsum eos id assumenda illo
           laudantium perspiciatis molestias asperiores numquam modi, laborum corporis 
@@ -83,10 +109,10 @@ const Main = () => {
           elit. Dignissimos dicta at accusantium iste nulla enim, reprehenderit perferendis 
           ex deleniti expedita ducimus voluptates natus quis, quos, non accusamus. Ut, autem
           reiciendis!
-        </motion.p>
+        </p>
 
-        <motion.h3 className="text-4xl mt-16">Lorem Ipsum</motion.h3>
-        <motion.p className="mt-4 w-[700px] ml-auto mr-auto">
+        <h3 className="text-4xl mt-16">Lorem Ipsum</h3>
+        <p className="mt-4 w-[700px] ml-auto mr-auto">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           Ducimus deleniti dicta corrupti eligendi, minima, ipsum eos id assumenda illo
           la udantium perspiciatis molestias asperiores numquam modi, laborum corporis
@@ -94,49 +120,91 @@ const Main = () => {
           elit. Dignissimos dicta at accusantium iste nulla enim, reprehenderit perferendis
           ex deleniti expedita ducimus voluptates natus quis, quos, non accusamus. Ut, autem
           reiciendis!
-        </motion.p>
+        </p>
       </motion.section>
 
-      <motion.section 
+      <section 
         style={{ backgroundImage: 'url("/assets/background.webp")', fontFamily: "Lora" }}
-        className="bg-center bg-no-repeat h-[93vh] w-full bg-fixed bg-cover"
+        className="bg-center bg-no-repeat w-full bg-fixed bg-cover"
       >
         <span>&nbsp;</span>
 
-        <motion.h1 className="text-center text-6xl mt-5 text-white">
+        <h1 className="text-center text-6xl mt-32 text-white">
           Recenze
-        </motion.h1>
+        </h1>
 
-        <motion.div className="flex justify-center">
+        <div className="flex justify-center">
           {[
             {
-
+              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+              "title": "Lorem Ipsum",
             },
             {
-
+              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+              "title": "Lorem Ipsum",
             }, 
             {
-
+              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+              "title": "Lorem Ipsum",
             }
           ].map((el, key) => {
             return (
-              <motion.div key={key} className="flex justify-center text-center p-2">
-                <motion.div className="w-[300px] h-fit bg-white mr-10 ml-10 mt-10 mb-10">
-                  <motion.div className="w-[300px] h-fit p-2 bg-white">
-                    <motion.p className="text-2xl mt-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </motion.p>
+              <motion.div 
+                key={key} 
+                className="flex justify-center text-center mb-32 p-2"
+                variants={describeVariants}
+                initial="offscreen"
+                whileInView="onscreen"
+                viewport={{ once: false, amount: 0.8 }}
+              >
+                <div className="w-[300px] h-fit bg-white mr-10 ml-10 mt-10 mb-10">
+                  <div className="w-[300px] h-fit p-5 bg-white">
+                    <p className="text-2xl mt-2 text-[#857e71]">
+                      { el.text }
+                    </p>
 
                     <span className="bg-[#857e71] inline-block w-[60px] h-1 mt-5">&nbsp;</span>
 
-                    <p>Neco tady</p>
-                  </motion.div>
-                </motion.div>
+                    <p>
+                      { el.title }
+                    </p>
+                  </div>
+                </div>
               </motion.div>
             ) 
           })}
-        </motion.div>
-      </motion.section>
+        </div>
+      </section>
+
+      <section className="bg-sky-900">
+        <div className="grid grid-cols-2 w-[1100px] ml-auto mr-auto">
+          <span>&nbsp;</span>
+
+          <div className="float-right text-right">
+            <button onClick={backToTop} className="bg-sky-700 text-sky-300 p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <h1 className="text-white text-center text-4xl mt-10">Lorem Ipsum</h1>
+
+        <div className="mt-3 mb-20 w-[500px] mr-auto ml-auto text-center outline-none">
+          <div className="inline">
+            <input 
+              type="text"
+              className="bg-sky-800 border-white outline-none border-solid border-3 w-[300px] text-white p-2 mt-5"
+              placeholder="Lorem Ipsum"
+            />
+
+            <button className="ml-3 bg-white text-sky-800 h-[40px] w-[90px]">Send</button>
+          </div>
+        </div>
+
+        <span className="h-30 w-full">&nbsp;</span>
+      </section>
     </div>
   )
 }
