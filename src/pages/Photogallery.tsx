a355370ae0f7f30077950e5b84db11092333a3cd
Atsukoro1 <dornicakkuba@gmail.com>
2022-11-18 19:30:50 +0100

    feat: 🎸 New

--- src/pages/Photogallery.tsx
+++ src/pages/Photogallery.tsx
@@ -7,16 +7,21 @@ const Photogallery = () => {
     return (
       <>
         <div className={`${imageOpen ? "fixed" : "hidden"} w-full fixed top-0 h-full`}>
-            <img 
-                className="w-[500px] fixed z-10 h-[300px] object-cover"
-                src={`/assets/photogallery/foto${image}.jpg`}
-                alt="Foto" 
-            />
+            <div className="bg-white h-fit m-auto mx-auto inset-x-0 w-[700px] rounded-lg fixed top-40 z-10">
+                <img 
+                    src={`/assets/photogallery/foto${image}.jpg`}
+                    alt="focused-image" 
+                    className="object-cover"
+                />
+            </div>
 
-            <div className="bg-black w-full h-full opacity-50"/>
+            <div 
+                className="bg-black w-full h-full opacity-50"
+                onClick={() => setImageOpen(false)}
+            />
         </div>
 
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 sm:w-[300px] md:w-[700px] lg:w-[1350px] ml-auto mr-auto">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:w-[300px] md:w-[700px] lg:w-[1000px] ml-auto mr-auto">
             {[Array(16).fill(0).map((el, key) => {
                 let klic = key + 1
                 return (
